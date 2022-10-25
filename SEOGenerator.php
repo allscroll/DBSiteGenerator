21bd525140c2127946472c5417984e6375554b34
allscroll <77875462+allscroll@users.noreply.github.com>
2022-10-25 16:25:54 +0700

    Create SEOGenerator.php

---
+++ SEOGenerator.php
@@ -0,0 +1,45 @@
+<?php
+
+class SEOGen
+{
+    /**
+     * Генерация заголовка
+     */
+    public static function getTitle( string $key ): string
+    {
+        $prefix = [
+            '',
+        ];
+        $postfix = [
+            '',
+        ];
+
+        shuffle( $prefix );
+        shuffle( $postfix );
+
+        return $prefix[ array_rand( $prefix ) ] .' '. $key .' '. $postfix[ array_rand( $postfix ) ];
+    }
+
+    /**
+     * Генерация seo description
+     */
+    public static function getSeoDescription( string $text ): string
+    {
+
+    }
+
+    /**
+     * Генерация seo keywords
+     */
+    public static function getSeoDescription( string $text ): string
+    {
+        $templates = [];
+
+        shuffle( $templates );
+        $randNum = mt_rand(3, 5);
+
+        array_rand( $prefix, $randNum );
+
+
+    }
+}
